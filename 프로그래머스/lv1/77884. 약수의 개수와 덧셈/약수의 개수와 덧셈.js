6eadb40f19b8139591b8ddf3fdaffb6b843a8578
Taehyun Kim <127714273+taehyunkim3@users.noreply.github.com>
2023-06-21 20:12:37 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.59 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,25 +1,4 @@
 function solution(left, right) {
-    const max = Math.max(left, right);
-    const min = Math.min(left, right);
-    let arr = [];
-
-    for (let i = min; i <= max; i++) {
-        arr.push(i);
-    }
-
-    let odd = 0;
-    let even = 0;
-
-    for (n of arr) {
-        let count = 1;
-        for (let j = 2; j <= n; j++) {
-            if (n % j === 0) count++;
-        }
-        count % 2 ? odd += n : even += n;
-    }
-
-
-
-    return even - odd;
+    var answer = 0;
+    return answer;
 }
-
